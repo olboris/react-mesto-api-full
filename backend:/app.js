6e137b0c12b947e3c8f4a3073bf35f7658ae1565
Babushkina O <boriskinaos@mail.ru>
2021-04-24 17:50:33 +0300

    обработка ошибки cors8

--- backend:/app.js
+++ backend:/app.js
@@ -59,7 +59,7 @@ app.use(function(req, res, next) {
   next();
 })
 
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
