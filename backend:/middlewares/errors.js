8d31c4a310ad67166f962d3d87612b0cda5b44b6
Babushkina O <boriskinaos@mail.ru>
2021-04-26 19:41:48 +0300

    исправление ошибки удаления карточки

--- backend:/middlewares/errors.js
+++ backend:/middlewares/errors.js
@@ -3,7 +3,7 @@ module.exports = (err, req, res, next) => {
   res.status(statusCode).send({
     message: statusCode === 500
       ? 'На сервере произошла ошибка'
-      : message,
+      : console.log('Упс'),
   });
   next();
 };
--- backend:/routes/users.js
+++ backend:/routes/users.js
@@ -11,8 +11,8 @@ router.get('/me', getUserInfo);
 
 router.patch('/me', celebrate({
   body: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
-    about: Joi.string().min(2).max(30),
+    name: Joi.string().required().min(2).max(30),
+    about: Joi.string().required().min(2).max(30),
   }),
 }), updateUser);
 
